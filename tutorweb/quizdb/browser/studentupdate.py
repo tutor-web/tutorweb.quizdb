ba1aa453be1632ab7749e6addc303ea70076b7ac
Jamie Lentin <jm@lentin.co.uk>
2013-10-03 09:49:59 +0100

    Add view to update all students

---
+++ tutorweb/quizdb/browser/studentupdate.py
@@ -0,0 +1,22 @@
+# import logging
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
+
+from z3c.saconfig import Session
+
+from Products.CMFCore.utils import getToolByName
+
+from tutorweb.quizdb import db
+from .base import JSONBrowserView
+
+
+class StudentUpdateView(JSONBrowserView):
+    """Update all student email addresses"""
+
+    def asDict(self):
+        """For all students already in DB, update details"""
+        mtool = getToolByName(self.context, 'portal_membership')
+        for dbStudent in Session.query(db.Student).all():
+            mb = mtool.getMemberById(dbStudent.userName)
+            dbStudent.eMail = mb.getProperty('email')
+        Session.flush()
+        return dict(success=True)
---
+++ tutorweb/quizdb/tests/test_browser_studentupdate.py
@@ -0,0 +1,68 @@
+from z3c.saconfig import Session
+from zope.testing.loggingsupport import InstalledHandler
+
+from plone.app.testing import login
+
+from Products.CMFCore.utils import getToolByName
+
+from tutorweb.quizdb import db
+from .base import IntegrationTestCase
+from .base import MANAGER_ID, USER_A_ID, USER_B_ID, USER_C_ID
+
+
+class StudentUpdateViewTest(IntegrationTestCase):
+    def setUp(self):
+        """Set up a class ready for testing"""
+        self.loghandlers = dict(
+            sqlalchemy=InstalledHandler('sqlalchemy.engine'),
+            sync=InstalledHandler('tutorweb.quizdb.browser.sync')
+        )
+
+    def logs(self, name='sqlalchemy'):
+        return [x.getMessage() for x in self.loghandlers[name].records]
+
+    def test_studentUpdate(self):
+        """Should be able to update all student's email addresses in one go"""
+        portal = self.layer['portal']
+
+        # Login as user B and C, to get them in the DB
+        login(portal, USER_A_ID)
+        self.assertEqual(self.getCurrentStudent().userName, USER_A_ID)
+        login(portal, USER_C_ID)
+        self.assertEqual(self.getCurrentStudent().userName, USER_C_ID)
+
+        # Change all email addresses, run update
+        login(portal, MANAGER_ID)
+        mtool = getToolByName(portal, 'portal_membership')
+        mtool.getMemberById(USER_A_ID).setMemberProperties(dict(
+            email='alfred@example.com',
+        ))
+        mtool.getMemberById(USER_B_ID).setMemberProperties(dict(
+            email='bert@example.com',
+        ))
+        mtool.getMemberById(USER_C_ID).setMemberProperties(dict(
+            email='catherine@example.com',
+        ))
+        update = portal.restrictedTraverse('quizdb-student-update').asDict()
+        self.assertEqual(update['success'], True)
+
+        # Email addresses have already changed, apart from B who wasn't there
+        for dbStudent in Session.query(db.Student).all():
+            self.assertEqual(
+                'alfred@example.com' if dbStudent.userName == USER_A_ID
+                else 'Betty@example.com' if dbStudent.userName == USER_B_ID
+                else 'catherine@example.com',
+                dbStudent.eMail,
+            )
+            pass
+
+        # Set email addresses back again
+        for id in [USER_A_ID, USER_B_ID, USER_C_ID]:
+            mtool.getMemberById(id).setMemberProperties(dict(
+                email=id + '@example.com',
+            ))
+
+    def getCurrentStudent(self):
+        """Use getCurrentStudent() to create students"""
+        lec = self.layer['portal']['dept1']['tut1']['lec1']
+        return lec.restrictedTraverse('quizdb-sync').getCurrentStudent()
