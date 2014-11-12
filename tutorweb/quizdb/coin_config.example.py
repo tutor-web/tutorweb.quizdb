ac913dfa6e54235269f8d5dff02105d622ffdf2b
Jamie Lentin <jm@lentin.co.uk>
2014-11-12 10:23:41 +0000

    The testnet isn't actually that useful

--- tutorweb/quizdb/coin_config.example.py
+++ tutorweb/quizdb/coin_config.example.py
@@ -1,6 +1,4 @@
-from Globals import DevelopmentMode
-
 RPC_HOST = "127.0.0.1"
-RPC_PORT = 24242 if DevelopmentMode else 14242  # Use testnet whilst in development mode
+RPC_PORT = 14242
 RPC_USER = "smileycoinrpc"
 RPC_PASS = "MOOMOOMOOMOOMOOMOOOOOO"
