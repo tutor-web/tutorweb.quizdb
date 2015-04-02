d187ccb8d4ad802d891b50d46b917e25ef80431a
Jamie Lentin <jm@lentin.co.uk>
2015-04-02 13:55:52 +0100

    Split helpers out of JSONBrowserView

--- tutorweb/quizdb/browser/base.py
+++ tutorweb/quizdb/browser/base.py
@@ -18,68 +18,7 @@ from Products.Five.browser import BrowserView
 from tutorweb.quizdb import db
 
 
-class JSONBrowserView(BrowserView):
-    def asDict(self, data):
-        """Return dict to be turned into JSON"""
-        raise NotImplementedError
-
-    def __call__(self):
-        try:
-            # Is there a request body?
-            if self.request.get_header('content_length') > 0:
-                # NB: Should be checking self.request.getHeader('Content-Type') ==
-                # 'application/json' but zope.testbrowser cannae do that.
-                self.request.stdin.seek(0)
-                data = json.loads(self.request.stdin.read())
-            else:
-                data = None
-
-            out = self.asDict(data)
-            self.request.response.setStatus(200)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(out)
-        except Unauthorized, ex:
-            self.request.response.setStatus(403)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
-                error=ex.__class__.__name__,
-                message=str(ex),
-            ))
-        except Redirect, ex:
-            # Return as Unauthorized, so Javascript can redirect full page
-            self.request.response.setStatus(403)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
-                error=ex.__class__.__name__,
-                location=str(ex),
-            ))
-        except NotFound, ex:
-            self.request.response.setStatus(404)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
-                error=ex.__class__.__name__,
-                message=str(ex),
-            ))
-        except BadRequest, ex:
-            self.request.response.setStatus(400)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
-                error=ex.__class__.__name__,
-                message=str(ex),
-            ))
-        except Exception, ex:
-            if DevelopmentMode:
-                import traceback
-            logging.error("Failed call: " + self.request['URL'])
-            logging.exception(ex)
-            self.request.response.setStatus(500)
-            self.request.response.setHeader("Content-type", "application/json")
-            return json.dumps(dict(
-                error=ex.__class__.__name__,
-                message=str(ex),
-                stacktrace=traceback.format_exc() if DevelopmentMode else '',
-            ))
-
+class BrowserViewHelpers(object):
     def getCurrentStudent(self):
         """Try fetching the current student, create if they don't exist"""
         membership = self.context.portal_membership
@@ -154,3 +93,65 @@ class JSONBrowserView(BrowserView):
             pu = getToolByName(self.context, "portal_url")
             self._portal = pu.getPortalObject()
         return self._portal
+
+class JSONBrowserView(BrowserView, BrowserViewHelpers):
+    def asDict(self, data):
+        """Return dict to be turned into JSON"""
+        raise NotImplementedError
+
+    def __call__(self):
+        try:
+            # Is there a request body?
+            if self.request.get_header('content_length') > 0:
+                # NB: Should be checking self.request.getHeader('Content-Type') ==
+                # 'application/json' but zope.testbrowser cannae do that.
+                self.request.stdin.seek(0)
+                data = json.loads(self.request.stdin.read())
+            else:
+                data = None
+
+            out = self.asDict(data)
+            self.request.response.setStatus(200)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(out)
+        except Unauthorized, ex:
+            self.request.response.setStatus(403)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(dict(
+                error=ex.__class__.__name__,
+                message=str(ex),
+            ))
+        except Redirect, ex:
+            # Return as Unauthorized, so Javascript can redirect full page
+            self.request.response.setStatus(403)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(dict(
+                error=ex.__class__.__name__,
+                location=str(ex),
+            ))
+        except NotFound, ex:
+            self.request.response.setStatus(404)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(dict(
+                error=ex.__class__.__name__,
+                message=str(ex),
+            ))
+        except BadRequest, ex:
+            self.request.response.setStatus(400)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(dict(
+                error=ex.__class__.__name__,
+                message=str(ex),
+            ))
+        except Exception, ex:
+            if DevelopmentMode:
+                import traceback
+            logging.error("Failed call: " + self.request['URL'])
+            logging.exception(ex)
+            self.request.response.setStatus(500)
+            self.request.response.setHeader("Content-type", "application/json")
+            return json.dumps(dict(
+                error=ex.__class__.__name__,
+                message=str(ex),
+                stacktrace=traceback.format_exc() if DevelopmentMode else '',
+            ))
