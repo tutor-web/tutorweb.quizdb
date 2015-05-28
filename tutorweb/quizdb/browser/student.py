693d97943ef759f4f5e520edf26d80d6b0c6b1ef
Jamie Lentin <jm@lentin.co.uk>
2015-05-28 18:07:15 +0100

    Add view to let javascript client modify currently logged-in user

--- tutorweb/quizdb/browser/student.py
+++ tutorweb/quizdb/browser/student.py
@@ -1,12 +1,13 @@
 # import logging
 # logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
+from AccessControl import Unauthorized
 
 from z3c.saconfig import Session
 
 from Products.CMFCore.utils import getToolByName
 
 from tutorweb.quizdb import db
-from .base import JSONBrowserView
+from .base import JSONBrowserView, BrowserViewHelpers
 
 
 class StudentUpdateView(JSONBrowserView):
@@ -20,3 +21,40 @@ class StudentUpdateView(JSONBrowserView):
             dbStudent.eMail = mb.getProperty('email')
         Session.flush()
         return dict(success=True)
+
+
+class StudentUpdateDetailsView(JSONBrowserView, BrowserViewHelpers):
+    """Update / return all current student details"""
+
+    def asDict(self, data):
+        """Retuun user details, optionally updating first"""
+        mtool = getToolByName(self.context, 'portal_membership')
+        if mtool.isAnonymousUser():
+            raise Unauthorized('You are not logged in')
+        mb = mtool.getAuthenticatedMember()
+
+        # If we were given any data, update existing records
+        if data:
+            reg_tool = getToolByName(self.context, 'portal_registration')
+            newProps = {}
+            for d in data:
+                if d['name'] == 'fullname':
+                    newProps[d['name']] = d['value']
+                elif d['name'] == 'email':
+                    if not reg_tool.isValidEmail(d['value']):
+                        raise ValueError("Email address %s not valid" % d['value'])
+                    newProps[d['name']] = d['value']
+                elif d['name'] == 'accept':
+                    newProps[d['name']] = True
+
+            # Update member & fetch DB entry (to update DB)
+            mb.setMemberProperties(mapping=newProps)
+            self.getCurrentStudent()
+
+        # Display current data
+        return dict(
+            username = mb.getUserName(),
+            fullname = mb.getProperty('fullname', ''),
+            email = mb.getProperty('email', ''),
+            accept = mb.getProperty('accept', False)
+        )
--- tutorweb/quizdb/tests/test_browser_student.py
+++ tutorweb/quizdb/tests/test_browser_student.py
@@ -66,3 +66,65 @@ class StudentUpdateViewTest(IntegrationTestCase):
         """Use getCurrentStudent() to create students"""
         lec = self.layer['portal']['dept1']['tut1']['lec1']
         return lec.restrictedTraverse('quizdb-sync').getCurrentStudent()
+
+
+class StudentUpdateDetailsViewTest(IntegrationTestCase):
+    def test_asDict(self):
+        """Should be able to fetch data & update it"""
+        portal = self.layer['portal']
+
+        login(portal, MANAGER_ID)
+        mtool = getToolByName(portal, 'portal_membership')
+        mtool.getMemberById(USER_A_ID).setMemberProperties(dict(
+            fullname='David Arnold',
+        ))
+        mtool.getMemberById(USER_B_ID).setMemberProperties(dict(
+            fullname='Betty Boop',
+            accept=False,
+        ))
+
+        login(portal, USER_A_ID)
+        details = portal.restrictedTraverse('quizdb-student-updatedetails').asDict(None)
+        self.assertEquals(details, dict(
+            username='Arnold',
+            email='Arnold@example.com',
+            fullname='David Arnold',
+            accept=True,
+        ))
+
+        login(portal, USER_B_ID)
+        details = portal.restrictedTraverse('quizdb-student-updatedetails').asDict(None)
+        self.assertEquals(details, dict(
+            username='Betty',
+            email='Betty@example.com',
+            fullname='Betty Boop',
+            accept=False,
+        ))
+
+        # Try updating and make sure we still get the same data back
+        details = portal.restrictedTraverse('quizdb-student-updatedetails').asDict([
+            dict(name="fullname", value="Sweaty Betty"),
+            dict(name="email", value="sb@gmail.com"),
+            dict(name="accept", value="whatever"),
+        ])
+        self.assertEquals(details, dict(
+            username='Betty',
+            email='sb@gmail.com',
+            fullname='Sweaty Betty',
+            accept=True,
+        ))
+        self.assertEquals(details, portal.restrictedTraverse('quizdb-student-updatedetails').asDict(None))
+
+        # Invalid email addresses generate an error & nothing is changed
+        with self.assertRaisesRegexp(ValueError, 'nonsense'):
+            portal.restrictedTraverse('quizdb-student-updatedetails').asDict([
+                dict(name="email", value="nonsense"),
+            ])
+        self.assertEquals(details, portal.restrictedTraverse('quizdb-student-updatedetails').asDict(None))
+
+        # Member has actually been updated
+        login(portal, MANAGER_ID)
+        mb = mtool.getMemberById(USER_B_ID)
+        self.assertEqual(mb.getProperty('email'), 'sb@gmail.com')
+        self.assertEqual(mb.getProperty('fullname'), 'Sweaty Betty')
+        self.assertEqual(mb.getProperty('accept'), True)
