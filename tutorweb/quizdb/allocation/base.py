4ee2b8ee04243194e13ad6ed7ec93e6bdc3ddf9a
Jamie Lentin <jm@lentin.co.uk>
2016-04-08 12:53:08 +0100

    Pull allocation_method out of lecture settings

--- tutorweb/quizdb/allocation/base.py
+++ tutorweb/quizdb/allocation/base.py
@@ -1,12 +1,35 @@
+import re
+import importlib
+
+from z3c.saconfig import Session
+
+from tutorweb.quizdb import db
+
 DEFAULT_QUESTION_CAP = 100  # Maximum number of questions to assign to user
+
+
+def allocation_module(mod_name):
+    """Load an allocation module and return the allocation class within"""
+    mod_name = re.sub('[^A-Z]+', '', mod_name, 0, re.I)
+    return getattr(importlib.import_module(
+        '..%s' % (mod_name),
+        __name__,
+    ), '%sAllocation' % mod_name.title())
+
 
 class Allocation(object):
     @classmethod
     def allocFor(cls, student, dbLec, urlBase="/"):
-        from .original import OriginalAllocation
+        """Return the correct Allocation Method instance for this lecture"""
+        alloc_method = (Session.query(db.LectureSetting.value)
+            .filter(db.LectureSetting.lectureId == dbLec.lectureId)
+            .filter(db.LectureSetting.studentId == student.studentId)
+            .filter(db.LectureSetting.key == 'allocation_method')
+            .first())
+        if not alloc_method:
+            alloc_method = ['original']
 
-        klass = OriginalAllocation  # There is only one
-        return klass(
+        return allocation_module(alloc_method[0])(
             student=student,
             dbLec=dbLec,
             urlBase=urlBase,
