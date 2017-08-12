216d5f29b5549d32d9afd88d683a2f57b7f0a22d
Jamie Lentin <jm@lentin.co.uk>
2017-08-12 09:38:43 +0000

    Datbase changes up to ef723f57bdd7b06885d74e013cf3ff0a33e61051

--- db_upgrades.sql
+++ db_upgrades.sql
@@ -1,2 +1,46 @@
 -- 698d76d238223b18e794c8021b5e6158db82c0d8
 ALTER TABLE question ADD incorrectChoices varchar(128) NOT NULL;
+
+-- ef723f57bdd7b06885d74e013cf3ff0a33e61051
+ALTER TABLE lecture ADD `currentVersion` int(11) NOT NULL;
+ALTER TABLE question ADD `incorrectChoices` varchar(128) NOT NULL;
+CREATE TABLE `lectureGlobalSetting` (
+  `lectureId` int(11) NOT NULL,
+  `lectureVersion` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(100) COLLATE utf8_bin NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `value` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `shape` float DEFAULT NULL,
+  `max` float DEFAULT NULL,
+  `min` float DEFAULT NULL,
+  PRIMARY KEY (`lectureId`,`lectureVersion`,`key`),
+  KEY `idx_autoinc_lectureVersion` (`lectureVersion`),
+  CONSTRAINT `lectureGlobalSetting_ibfk_1` FOREIGN KEY (`lectureId`) REFERENCES `lecture` (`lectureId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `lectureStudentSetting` (
+  `lectureId` int(11) NOT NULL,
+  `lectureVersion` int(11) NOT NULL,
+  `studentId` int(11) NOT NULL,
+  `key` varchar(100) COLLATE utf8_bin NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `value` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`lectureId`,`lectureVersion`,`studentId`,`key`),
+  KEY `lectureVersion` (`lectureVersion`),
+  KEY `studentId` (`studentId`),
+  CONSTRAINT `lectureStudentSetting_ibfk_1`
+    FOREIGN KEY (`lectureId`, `lectureVersion`)
+    REFERENCES `lectureGlobalSetting` (`lectureId`, `lectureVersion`),
+  CONSTRAINT `lectureStudentSetting_ibfk_2`
+    FOREIGN KEY (`lectureId`)
+    REFERENCES `lecture` (`lectureId`),
+  CONSTRAINT `lectureStudentSetting_ibfk_3`
+    FOREIGN KEY (`lectureVersion`)
+    REFERENCES `lectureGlobalSetting` (`lectureVersion`),
+  CONSTRAINT `lectureStudentSetting_ibfk_4`
+    FOREIGN KEY (`studentId`)
+    REFERENCES `student` (`studentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ALTER TABLE answer ADD `lectureVersion` int(11) DEFAULT NULL;
+ALTER TABLE answer ADD CONSTRAINT `answer_ibfk_x`
+    FOREIGN KEY (`lectureId`, `lectureVersion`)
+    REFERENCES `lectureGlobalSetting` (`lectureId`, `lectureVersion`);
