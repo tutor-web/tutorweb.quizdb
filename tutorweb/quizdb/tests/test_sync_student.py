f689921614c47badad90e93f8bc49211ed266fa6
Jamie Lentin <jm@lentin.co.uk>
2017-10-02 09:16:44 +0000

    sync/student: Not "3.0" is important for cap_, which wants int()able values

--- tutorweb/quizdb/tests/test_sync_student.py
+++ tutorweb/quizdb/tests/test_sync_student.py
@@ -39,7 +39,7 @@ class SyncStudentIntegration(IntegrationTestCase):
         with self.assertRaisesRegexp(ValueError, 'iaa_mode'):
             csv(key="iaa_mode", value="fun-size", max=4)
 
-        # Integer settings get rounded
+        # Integer settings get rounded, don't have "3.0" at end
         for x in xrange(LOTS_OF_TESTS):
             self.assertIn(csv(key="grade_nmin", max=9), '0 1 2 3 4 5 6 7 8 9'.split())
 
